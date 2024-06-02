361015db4129aba86db2da0122ab01f366a3d1ca
SangWon7242 <gotkdqja@gmail.com>
2024-06-02 11:14:18 +0900

    useState를 사용하여 숫자 증가 및 감소 기능 구현

--- app/page.js
+++ app/page.js
@@ -1,28 +1,17 @@
-// props는 properties의 줄임말
-// 특정 값을 컴포넌트의 전달해줄 때 사용
-const Hello = (props) => {
-  return <div>안녕하세요 {props.name}</div>;
-};
+"use client";
 
-// 여러개의 props 넘기기, 비구조화 할당(구조분해)
-// 매번 color: props.color --> 매번 props를 붙이는게 번거로움
-// ({ name, color }) 이렇게 props 대신 사용 가능
-const Good = ({ name, color }) => {
-  return <div style={{ color }}>안녕하세요 {name}</div>;
-};
+import { useState } from "react";
 
 export default function Home() {
-  const style = {
-    backgroundColor: "red",
-    width: "200px",
-    height: "200px",
-    marginTop: "20px",
-  };
+  const [no, setNo] = useState(0);
 
   return (
     <div>
-      <Hello name="react" />
-      <Good name="react" color="blue" />
+      <h1>no : {no}</h1>
+      <div>
+        <button onClick={() => setNo(no + 1)}>증가</button>
+        <button onClick={() => setNo(no - 1)}>감소</button>
+      </div>
     </div>
   );
 }
